3491a2acea037afe543f71bfad265cddd33adb02
HypeST11 <assasinogamez@gmail.com>
2025-04-04 15:28:30 -0300

    Update credentials.py

--- examples/credentials/credentials.py
+++ examples/credentials/credentials.py
@@ -2,8 +2,8 @@
 
 # Insira aqui suas credenciais
 CREDENTIALS = {
-    'client_id': 'client_id',
-    'client_secret': 'client_secret',
+    'client_id': 'Client_Id_d1effcc79147f9eedadf7d68bdc905e698df3273',
+    'client_secret': 'Client_Secret_13e2a2831a18a22b4654d1c6947f1d1300c6bf48',
     'sandbox': True, # True: Ambiente de Homologação |  False: Ambiente de Produção
-    'certificate': 'insira-o-caminho-completo-do-certificado'
+    'certificate': 'C:/Users/RPSafety/Downloads/conversor-p12-efi-main/conversor-p12-efi-main/producao-741293-teste 2_cert.pem'
 }
